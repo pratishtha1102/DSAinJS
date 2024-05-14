0c7373e9b5d7e579da368e198b42cbfa45bc337c
Pratishtha Negi <pratishthanegi@iitb.ac.in>
2024-05-14 16:32:01 +0200

    Added Solution - GfG to GitHub

---
+++ Easy/Anagram/anagram.js
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_line = readLine().split(' ');
+        let a = input_line[0];
+        let b = input_line[1];
+        let obj = new Solution();
+        if(obj.isAnagram(a, b))
+            console.log("YES");
+        else
+            console.log("NO");
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @returns {boolean}
+*/
+ 
+class Solution 
+{
+    isAnagram(a, b) {
+        if (a.length != b.length) return false;
+        
+        const aSorted = a.split('').sort();
+        const bSort = b.split('').sort();
+        for (let i=0; i<=a.length; i++) {
+            if (aSort[i] != bSort[i]) return false;
+        }
+        return true;
+    }
+}
