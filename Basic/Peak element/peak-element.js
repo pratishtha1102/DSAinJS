094a1f476c1ecf3e5cdbd821c47e838e6ad84075
Pratishtha Negi <pratishthanegi@iitb.ac.in>
2024-05-15 16:05:48 +0200

    Added Solution - GfG to GitHub

---
+++ Basic/Peak element/peak-element.js
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let a = new Array(n);
+        let tmp = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            a[i] = input_ar1[i];
+            tmp[i] = a[i];
+        }
+        let obj = new Solution();
+        let A = obj.peakElement(tmp, n);
+        let f=0;
+        if(n==1)
+		    f=1;
+		else if(A==0 && a[0]>=a[1])
+		    f=1;
+		else if(A==n-1 && a[n-1]>=a[n-2])
+		    f=1;
+		else if(a[A] >=a[A+1] && a[A]>= a[A-1])
+		    f=1;
+		else
+		    f=0;
+		console.log(f);
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution {
+    peakElement(arr, n) {
+        for (let i = 0; i < n; i++) {
+            // Check if arr[i] is greater than or equal to its left and right neighbors (if they exist)
+            if ((i === 0 || arr[i] >= arr[i - 1]) && (i === n - 1 || arr[i] >= arr[i + 1])) {
+                // If yes, return the index of the peak element
+                return i;
+            }
+        }
+        // If no peak element is found, return -1
+        return -1;
+    }
+
+}
